3bc705ea4b331efb61bcb8906107482cff251e7e
Антон Домолазов <anton@domelaz.ru>
2016-04-26 16:10:01 +0300

    references.ts exposed
    `references.ts` генерируется Грантом, и место ему в .gitignore
    Но оставлен для людей, у которых аллергия на консоль.

---
+++ references.ts
@@ -0,0 +1,8 @@
+//grunt-start
+/// <reference path="typings/adobe/cs3/extended-cs3.d.ts" />
+/// <reference path="typings/adobe/cs3/illustrator-cs3.d.ts" />
+/// <reference path="typings/adobe/cs3/lib.core.d.ts" />
+/// <reference path="typings/lodash/lodash.d.ts" />
+/// <reference path="typings/lodash/splitter.d.ts" />
+/// <reference path="src/index.ts" />
+//grunt-end
